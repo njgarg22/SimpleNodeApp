62de30bb07e55453700ada5fab21e3b2698fca28
njgarg22 <njgarg22@gmail.com>
2020-04-29 01:13:47 +0530

    Second commit

--- simplenodeapp/app.js
+++ simplenodeapp/app.js
@@ -4,5 +4,5 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 app.listen(4000, function () {
-  console.log('Example app listening on port 4000!');
+  console.log('Simple app listening on port 4000!');
 });
